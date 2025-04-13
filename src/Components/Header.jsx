c8c50d147de72722329381b4aa41405757d24da6
R-S-A-N-J-A-Y <sanjay638291@gmail.com>
2025-04-13 21:53:51 +0530

    Header Animation is completed.

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,22 +1,42 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { NavLink, useLocation } from "react-router-dom";
 
 const Header = () => {
+  const location = useLocation();
+  const [url, setUrl] = useState("/");
+
+  useEffect(() => {
+    setUrl(location.pathname);
+  }, [location]);
+
   return (
     <nav className="navbar navbar-expand-sm header">
       <div className="container-fluid d-flex justify-content-center gap-5">
-        <NavLink to="/" className="nav-link">
-          Home
-        </NavLink>
-        <NavLink to="/stats" className="nav-link">
-          Statistics
-        </NavLink>
-        <NavLink to="/addmovie" className="nav-link">
-          Add Movie
-        </NavLink>
-        <NavLink to="/wishlist" className="nav-link">
-          My WishList
-        </NavLink>
+        <div className="d-flex flex-column align-items-center gap-2">
+          <NavLink to="/" className="nav-link">
+            Home
+          </NavLink>
+          {url === "/" && <div className="line"></div>}
+        </div>
+
+        <div className="d-flex flex-column align-items-center gap-2">
+          <NavLink to="/stats" className="nav-link">
+            Statistics
+          </NavLink>
+          {url === "/stats" && <div className="line"></div>}
+        </div>
+        <div className="d-flex flex-column align-items-center gap-2">
+          <NavLink to="/addmovie" className="nav-link">
+            Add Movie
+          </NavLink>
+          {url === "/addmovie" && <div className="line"></div>}
+        </div>
+        <div className="d-flex flex-column align-items-center gap-2">
+          <NavLink to="/wishlist" className="nav-link">
+            My WishList
+          </NavLink>
+          {url === "/wishlist" && <div className="line"></div>}
+        </div>
       </div>
     </nav>
   );
