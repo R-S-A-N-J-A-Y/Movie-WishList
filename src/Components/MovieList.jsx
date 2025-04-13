b79c85438d212177dcdb39a2c9d5a8f5bed87e05
R-S-A-N-J-A-Y <sanjay638291@gmail.com>
2025-04-13 21:40:32 +0530

    Font family for h tags

--- src/Components/MovieList.jsx
+++ src/Components/MovieList.jsx
@@ -5,9 +5,9 @@ const MovieList = () => {
   const { movies } = useMovie();
 
   return (
-    <div className="m-2 p-3">
-      <h1 className="text-center">Available Movies</h1>
-      <div className="m-0 row row-cols-5 g-3">
+    <div className="m-2 p-4 movie-list">
+      <h1 className="text-center m-2">Available Movies</h1>
+      <div className="m-0 row row-cols-5 gx-4 gy-5">
         {movies.map((movie) => (
           <div className="col">
             <MovieCard key={movie.id} movie={movie} />
--- src/Components/WishList.jsx
+++ src/Components/WishList.jsx
@@ -4,9 +4,9 @@ import MovieCard from "./MovieCard";
 const WishList = () => {
   const { Favorites } = useMovie();
   return (
-    <div className="m-2 p-3">
+    <div className="m-2 p-3 movie-list">
       <h1 className="text-center">My Favorites</h1>
-      <div className="m-0 row row-cols-5 g-3">
+      <div className="m-0 row row-cols-5 gx-4 gy-5">
         {Favorites.map((movie) => (
           <div className="col">
             <MovieCard key={movie.id} movie={movie} />
