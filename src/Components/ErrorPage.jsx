9734f2ff69ff1a1bee6f7a52c135527eb1d0f608
R-S-A-N-J-A-Y <sanjay638291@gmail.com>
2025-04-13 22:10:43 +0530

    production icon bug fix

--- src/Components/ErrorPage.jsx
+++ src/Components/ErrorPage.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const ErrorPage = () => {
   return (
     <div className="bg-white text-dark d-flex align-items-center error-page">
-      <img src="public/Icons/Oops.jpg" alt="" />
+      <img src="Icons/Oops.jpg" alt="" />
       <p>Oops this page is Not Available.</p>
     </div>
   );
--- src/Contexts/MovieContext.jsx
+++ src/Contexts/MovieContext.jsx
@@ -7,7 +7,7 @@ const initalMovies = [
     id: 1,
     title: "Avengers: Infinity War",
     director: "Anthony and Joe Russo",
-    moviePreview: "public/MoviePreview/InfinityWar.jpg",
+    moviePreview: "MoviePreview/InfinityWar.jpg",
     genre: "Superhero",
     rating: 9.3,
     isWatched: true,
