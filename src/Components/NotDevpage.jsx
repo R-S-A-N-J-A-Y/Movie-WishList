337fc5b775da37b5f09bedc0ef5ca34fa23403a1
R-S-A-N-J-A-Y <sanjay638291@gmail.com>
2025-05-05 11:48:45 +0530

    Add Responsiveness for Not Developed page.

--- src/Components/NotDevpage.jsx
+++ src/Components/NotDevpage.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const NotDevpage = () => {
   return (
-    <div className="bg-white text-dark d-flex align-items-center dev-page">
-      <img src="Icons/cons.png" alt="" />
+    <div className="bg-white text-dark d-flex flex-column flex-lg-row align-items-center dev-page gap-4">
+      <img src="Icons/cons.png" alt="" className="mb-3 mb-md-0" />
       <div>
         <p>This Page is Currently Under Construction.</p>
         <p>Try Again after Some Time.</p>
